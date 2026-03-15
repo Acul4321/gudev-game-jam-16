c4f44ea2a9eb3873a216347344d294407c607b2d
Cameron Early <contactcameronearly@gmail.com>
2026-03-15 21:34:53 +0000

    fixing mobile input (methinks)

--- time/menu/main_menu.gd
+++ time/menu/main_menu.gd
@@ -2,11 +2,17 @@ extends Node2D
 
 func _ready() -> void:
 	%enterButton.pressed.connect(_on_enter_button_pressed)
-	Music.play_song.emit(&"Time Loop Main Menu", true, false, 0.0)
+	%manuscriptEdit.text_submitted.connect(_on_manuscript_submitted)
+	Music.play(&"Time Loop Main Menu", true)
+
+func _on_manuscript_submitted(_new_text: String) -> void:
+	_on_enter_button_pressed()
 
 func _on_enter_button_pressed() -> void:
-	if %manuscriptEdit.text.strip_edges() == "":
+	var title: String = %manuscriptEdit.text.strip_edges()
+	if title == "":
 		Game.manuscript_title = "Forgottenly named Manuscript"
 	else:
-		Game.manuscript_title = %manuscriptEdit.text.strip_edges()
+		Game.manuscript_title = title
+
 	get_tree().change_scene_to_file("res://main/main.tscn")
