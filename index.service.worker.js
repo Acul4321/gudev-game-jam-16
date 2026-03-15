1cc9ccb7bfc0fab87a2fba7c1201e93f5db69e06
DastardlySky <DastardlySky@users.noreply.github.com>
2026-03-15 20:53:09 +0000

    Deploying to gh-pages from @ Acul4321/gudev-game-jam-16@dd55ec96b551745972b6b4bc2e361ee5cbd1cbb4 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773599097|21553525';
+const CACHE_VERSION = '1773607974|19979762';
 /** @type {string} */
 const CACHE_PREFIX = 'time-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
